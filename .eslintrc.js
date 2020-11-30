2d88ad39bd4454a7097c36372bbd0ea37baaf692
Your_Name <oleksiiyashchuk@MacBook-Pro-Oleksii.local>
2020-11-30 11:41:39 +0200

    add custom eslint / prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 125,
+      },
+    ],
+    'comma-dangle': [2, 'always-multiline'],
+  },
+  plugins: ['prettier'],
 };
